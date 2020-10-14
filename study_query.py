2063ca89fdc91b9444244eea215dec5ac14099ea
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2020-10-14 16:15:22 +0900

    Add study_query script

---
+++ study_query.py
@@ -0,0 +1,60 @@
+import argparse
+import sys
+
+import pandas as pd
+from pydicom.dataset import Dataset
+from logzero import logger
+import tqdm
+
+import qr
+import date_utils
+
+EXT_TABLE = {
+    '.csv': 'to_csv',
+    '.xlsx': 'to_excel',
+}
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Query by study instance UID.')
+    parser.add_argument('UID', help="Study instance UID", metavar='<UID>')
+    parser.add_argument('--output',
+                        help="Output filename. Default: <UID>.csv",
+                        metavar='<output>')
+    parser.add_argument(
+        '--loglevel',
+        help="Loglevel. default:%(default)s. choices:[%(choices)s]",
+        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+        default='INFO',
+        metavar='<level>')
+
+    args = parser.parse_args()
+
+    logger.setLevel(args.loglevel)
+
+    output_filename = args.output or '{}.csv'.format(args.UID)
+    logger.debug('Output filename:%s', output_filename)
+
+    attributes = [
+        'PatientID', 'Modality', 'StudyDate', 'StudyDescription',
+        'AccessionNumber', 'StudyInstanceUID'
+    ]
+
+    ds = Dataset()
+    for attr in attributes:
+        setattr(ds, attr, '')
+    ds.QueryRetrieveLevel = 'SERIES'
+    ds.StudyInstanceUID = args.UID
+
+    query_result = qr.query(ds, logger=logger)
+    df = pd.DataFrame([[getattr(r, attr) for attr in attributes]
+                       for r in query_result],
+                      columns=attributes)
+    logger.info('%d query results', len(df))
+    df.to_csv(output_filename, index=False)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
