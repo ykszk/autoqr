ce059aa86285cf7c4643ef09062838ebeef3a976
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2020-11-10 17:41:27 +0900

    Update bat scripts

--- scripts/debug_start.bat
+++ scripts/debug_start.bat
@@ -1,2 +1,2 @@
 call %USERPROFILE%\anaconda3\Scripts\activate.bat
-%USERPROFILE%\anaconda3\python.exe order_id.py --logfile debug-log.txt --loglevel DEBUG
+%USERPROFILE%\anaconda3\python.exe gui.py --logfile debug-log.txt --loglevel DEBUG
--- scripts/start.bat
+++ scripts/start.bat
@@ -1,2 +1,2 @@
 call %USERPROFILE%\anaconda3\Scripts\activate.bat
-%USERPROFILE%\anaconda3\python.exe autoqr.py --loglevel INFO
+%USERPROFILE%\anaconda3\python.exe gui.py --loglevel INFO
