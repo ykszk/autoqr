e3546bd8026408f07a11668402bf526232276638
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2020-11-30 20:03:28 +0900

    Remove unused code
    * QR code using pynetdicom is removed

--- qr.py
+++ qr.py
@@ -71,54 +71,6 @@ def query(ds: Dataset, server=None, aec=None, port=None, logger=None):
     return datasets
 
 
-def retrieve(ds, aec=None, port=None, logger=None):
-    logger = logger or default_logger
-    logger.debug('start retrieve')
-    if aec is None:
-        aec = settings.AECS[0]
-    if port is None:
-        port = settings.PORTS[0]
-
-    stored_datasets = []
-
-    def handle_store(event):
-        ds = event.dataset
-        ds.file_meta = event.file_meta
-
-        stored_datasets.append(ds)  # append is thread safe
-        return 0x0000
-
-    handlers = [(evt.EVT_C_STORE, handle_store)]
-
-    ae = AE(ae_title=settings.AETS[0])
-
-    ae.add_requested_context(PatientRootQueryRetrieveInformationModelGet)
-    ae.add_requested_context(CTImageStorage)
-    ae.add_requested_context(PositronEmissionTomographyImageStorage)
-    role_ct = build_role(CTImageStorage, scp_role=True)
-    role_pt = build_role(PositronEmissionTomographyImageStorage, scp_role=True)
-
-    assoc = ae.associate(settings.DICOM_SERVER,
-                         port,
-                         ext_neg=[role_ct, role_pt],
-                         evt_handlers=handlers,
-                         ae_title=aec)
-    if not assoc.is_established:
-        raise RuntimeError('Association rejected, aborted or never connected')
-
-    responses = assoc.send_c_get(ds,
-                                 PatientRootQueryRetrieveInformationModelGet)
-    for (status, _) in responses:
-        if not status:
-            raise RuntimeError(
-                'Connection timed out, was aborted or received invalid response'
-            )
-
-    assoc.release()
-    logger.debug('end retrieve %d', len(stored_datasets))
-    return stored_datasets
-
-
 def retrieve_dcmtk(ds,
                    outdir,
                    aet,
@@ -166,26 +118,6 @@ def retrieve_dcmtk(ds,
 
     logger.debug('end retrieve %s', ds.SeriesInstanceUID)
     return
-
-
-def qr(ds: Dataset, predicate=None, logger=None):
-    logger = logger or default_logger
-    found_datasets = query(ds, logger)
-    if predicate is not None:
-        found_datasets = [ds for ds in found_datasets if predicate(ds)]
-
-    all_datasets = []
-    for found_ds in found_datasets:
-        ds = Dataset()
-        ds.QueryRetrieveLevel = 'SERIES'
-        ds.PatientID = found_ds.PatientID
-        ds.AccessionNumber = found_ds.AccessionNumber
-        ds.StudyInstanceUID = found_ds.StudyInstanceUID
-        ds.SeriesInstanceUID = found_ds.SeriesInstanceUID
-        datasets = retrieve(ds, logger)
-        all_datasets.append(datasets)
-
-    return all_datasets
 
 
 def qr_dcmtk(ds: Dataset,
@@ -335,7 +267,8 @@ def main():
     ds.AccessionNumber = 'Ci5Lj86Rg4HrLuRLZjnqAA'
     ds.SeriesDescription = ''
 
-    all_datasets = qr(ds)
+    all_datasets = qr_dcmtk(ds, Path('.'), settings.AETS[0],
+                            settings.RECEIVE_PORTS[0])
     print(len(all_datasets))
 
 
